172d210589ff93e167b88f3ed79d519616ca0526
Brett Walker <brett@digitalmoksha.com>
2012-11-08 23:26:21 +0100

    add last_access date for users

--- core/config/routes.rb
+++ core/config/routes.rb
@@ -3,6 +3,7 @@ DmCore::Engine.routes.draw do
   scope ":locale" do
     namespace :admin do 
       match '/dashboard/index', :controller => 'dashboard', :action => :index, :as => 'dashboard'
+      match '/dashboard/update_site_assets', :controller => 'dashboard', :action => :update_site_assets
       resources :users
     end
   end
