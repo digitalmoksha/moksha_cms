473dea4c3ceb870d417da38a45538c3a81e3f281
Brett Walker <brett@digitalmoksha.com>
2013-03-06 18:54:09 +0100

    tweak

--- cms/app/helpers/dm_cms/render_helper.rb
+++ cms/app/helpers/dm_cms/render_helper.rb
@@ -1,4 +1,12 @@
 # To be overridden by application
 #------------------------------------------------------------------------------
-module DmCms::RenderHelper
+module DmCms
+  module RenderHelper
+
+    #------------------------------------------------------------------------------
+    def main_title
+      title = current_account.preferred_site_title
+      content_for?(:page_title) ? "#{content_for :page_title} | #{title}" : title
+    end
+  end
 end
