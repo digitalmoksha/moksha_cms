4dbdf80e35df69fed85717c0cbb3d87e245cdecf
Brett Walker <brett@digitalmoksha.com>
2013-03-06 18:56:08 +0100

    add site_title to account and default nls to the current_acccount prefix

--- core/config/routes.rb
+++ core/config/routes.rb
@@ -1,6 +1,7 @@
 #Rails.application.routes.prepend do 
 DmCore::Engine.routes.draw do
   scope ":locale" do
+    match '/admin', :controller => 'admin/dashboard', :action => :index
     namespace :admin do 
       match '/dashboard/index', :controller => 'dashboard', :action => :index, :as => 'dashboard'
       match '/dashboard/update_site_assets', :controller => 'dashboard', :action => :update_site_assets
--- core/lib/dm_core/nls.rb
+++ core/lib/dm_core/nls.rb
@@ -4,8 +4,10 @@ module Nls
   
   CONTINENTS = ['Europe', 'North America', 'South America', 'Oceania', 'Asia', 'Africa', 'Antartica']
   
+  # default scope the msg lookup to the account_prefix
   #------------------------------------------------------------------------------
   def nls(message_key, options = {})
+    options.reverse_merge!  :scope =>  Account.current.account_prefix
     I18n.t(message_key, options).html_safe
   end
 
