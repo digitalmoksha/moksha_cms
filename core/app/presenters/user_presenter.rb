c1f17dc6b16201d7ecffa15d51f778afae864e6d
Brett Walker <brett@digitalmoksha.com>
2013-03-25 17:17:22 +0100

    user labels

--- core/app/presenters/user_presenter.rb
+++ core/app/presenters/user_presenter.rb
@@ -3,4 +3,9 @@ class UserPresenter < BasePresenter
   presents :user
   #delegate :username, to: :user
 
+  #------------------------------------------------------------------------------
+  def role_label
+    user.is_admin? ? colored_label('Admin', :success) : (user.has_role?(:beta) ? colored_label('Beta', :warning) : (user.has_role?(:author) ? colored_label('Author', :info) : 'User' ))
+  end
+  
 end
