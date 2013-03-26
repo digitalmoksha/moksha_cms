9f341ac4602e4f4c67c61feb5bd68fb474d89a2d
Brett Walker <brett@digitalmoksha.com>
2013-03-27 00:29:55 +0100

    Don't set default, as Account.current not set yet when running in production

--- app/mailers/dm_core/site_mailer.rb
+++ app/mailers/dm_core/site_mailer.rb
@@ -2,13 +2,14 @@
 # All mailers should inherit from this class
 #------------------------------------------------------------------------------
 class DmCore::SiteMailer < ActionMailer::Base
-  default from:     Account.current.preferred_smtp_from_email,
-          reply_to: Account.current.preferred_smtp_from_email
 
   class DynamicSettingsInterceptor
      def self.delivering_email(message)
        message.delivery_method.settings.merge!(Account.current.smtp_settings)
+       message.from     = Account.current.preferred_smtp_from_email
+       message.reply_to = Account.current.preferred_smtp_from_email
      end
    end
    register_interceptor DynamicSettingsInterceptor
+
 end
