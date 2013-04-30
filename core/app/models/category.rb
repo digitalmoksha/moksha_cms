9f2793609df975a3edbb0028572ae2ed0ad65f20
Brett Walker <brett@digitalmoksha.com>
2013-05-01 01:25:05 +0200

    Add a core level category model

---
+++ core/app/models/category.rb
@@ -0,0 +1,19 @@
+#------------------------------------------------------------------------------
+class Category < ActiveRecord::Base
+
+  self.table_name       = 'core_categories'
+  attr_accessible       :name, :description
+
+  include RankedModel
+  ranks   :row_order
+
+  default_scope         { where(account_id: Account.current.id) }
+  scope                 :ordered, order('row_order ASC')
+
+  # --- globalize
+  translates            :name, :description, :fallbacks_for_empty_translations => true
+  globalize_accessors   :locales => DmCore::Language.language_array
+
+  validates_presence_of :name
+
+end
