9a7ac4a8bdb1cdcbd153de3f3c9df96ca9143d1d
Brett Walker <brett@digitalmoksha.com>
2013-05-24 21:30:41 +0200

    another fix to searching

--- app/datatables/user_datatable.rb
+++ app/datatables/user_datatable.rb
@@ -45,7 +45,7 @@ private
     users = User.includes(:user_profile => [ :country ] ).order("#{sort_column} #{sort_direction}")
     users = users.page(page).per_page(per_page)
     if params[:sSearch].present?
-      users = users.where("LOWER(user_profiles.first_name) like :search OR LOWER(user_profiles.last_name) like :search OR LOWER(email) like :search", search: "%#{params[:sSearch]}%")
+      users = users.where("LOWER(user_profiles.first_name) like :search OR LOWER(user_profiles.last_name) like :search OR LOWER(users.email) like :search", search: "%#{params[:sSearch]}%")
     end
     users
   end
