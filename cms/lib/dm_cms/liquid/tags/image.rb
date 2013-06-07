c40d14353cd63568c6ce72f0e8f5d9bf8321ef3f
Brett Walker <brett@digitalmoksha.com>
2013-06-07 12:25:46 +0200

    add 'protected' to image tag

--- cms/lib/dm_cms/liquid/tags/image.rb
+++ cms/lib/dm_cms/liquid/tags/image.rb
@@ -15,31 +15,40 @@ module Liquid
     #------------------------------------------------------------------------------
     def render(context)
       @attributes.reverse_merge!  'alt' =>  ''
-      src       = expand_url(@attributes["src"], "#{context.registers[:account_site_assets]}/images/")
-      mouseover = expand_url(@attributes["mouseover"], "#{context.registers[:account_site_assets]}/images/")
+      src       = file_path(@attributes["src"], context)
+      mouseover = file_path(@attributes["mouseover"], context)
 
       image_tag(src,  class: @attributes["class"], title: @attributes["title"], size: @attributes["size"], 
                       width: @attributes["width"], height: @attributes["height"], alt: @attributes["alt"],
                       style: @attributes["style"], id: @attributes["id"], mouseover: mouseover)
     end
   
-      def self.details
-        { name: self.tag_name,
-          summary: 'Displays an image',
-          description: "Displays an image. Will pull image from the site's images directory unless a full path is given.  All attributes are optional except <code>src</code>",
-          example: self.example,
-          category: 'image'
-        }
+    #------------------------------------------------------------------------------
+    def file_path(file_name, context)
+      if @attributes['protected'] == 'true' || @attributes['protected'] == 'yes' || @attributes['protected'] == '1'
+        expand_url(file_name, "/protected_asset/")
+      else
+        expand_url(file_name, "#{context.registers[:account_site_assets]}/images/")
       end
+    end
+    
+    def self.details
+      { name: self.tag_name,
+        summary: 'Displays an image',
+        description: "Displays an image. Will pull image from the site's images directory unless a full path is given.  All attributes are optional except <code>src</code>",
+        example: self.example,
+        category: 'image'
+      }
+    end
 
-      def self.example
-        example = <<-END_OF_STRING
-{% image src: placeholder_190x105.jpg, class: right, 
-         title: "Some title", size: 16x16, width: www, 
-         height: hhh, alt: "Alt text",
-         style : sss, id : iii, mouseover : mmm %}
+    def self.example
+      example = <<-END_OF_STRING
+{% image src: placeholder_190x105.jpg, class: right, protected: true,
+       title: "Some title", size: 16x16, width: www, 
+       height: hhh, alt: "Alt text",
+       style : sss, id : iii, mouseover : mmm %}
 END_OF_STRING
-      end
+    end
   end
   
   Template.register_tag('image', Image)
