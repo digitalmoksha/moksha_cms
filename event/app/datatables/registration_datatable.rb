62a789e5725236c1717f497a6f974df40917ed2b
Brett Walker <brett@digitalmoksha.com>
2013-06-28 21:53:41 +0200

    fixed issued where comparing a Money value to 0

--- event/app/datatables/registration_datatable.rb
+++ event/app/datatables/registration_datatable.rb
@@ -85,8 +85,8 @@ private
   #------------------------------------------------------------------------------
   def workshop_price(registration)
     if registration.workshop_price
-      color = (registration.balance_owed > 0) ? 'balance_owed' : 'balance_paid'
-      amount = (registration.balance_owed == 0) ? 'paid' : registration.balance_owed.format(:no_cents_if_whole => true)
+      color = (registration.balance_owed.cents > 0) ? 'balance_owed' : 'balance_paid'
+      amount = (registration.balance_owed.cents == 0) ? 'paid' : registration.balance_owed.format(:no_cents_if_whole => true)
       "<span data-placement='left' class='hovertip #{color}' title='#{registration.workshop_price.price.format(:no_cents_if_whole => true)} &mdash; #{registration.workshop_price.price_description}'>#{amount}</span>".html_safe
     else
       '-'
