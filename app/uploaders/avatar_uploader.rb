66e95ee0652f7d20b08fde2e81678ed3529dc0e1
Brett Walker <brett@digitalmoksha.com>
2013-07-18 23:19:30 +0200

    tweak

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -73,27 +73,3 @@ protected
   end
 
 end
-
-#------------------------------------------------------------------------------
-# Put the version name at the end of the file, instead of beginning
-# https://github.com/carrierwaveuploader/carrierwave/wiki/How-To:-Move-version-name-to-end-of-filename,-instead-of-front
-#------------------------------------------------------------------------------
-module CarrierWave
-  module Uploader
-    module Versions
-      def full_filename(for_file)
-        parent_name = super(for_file)
-        ext         = File.extname(parent_name)
-        base_name   = parent_name.chomp(ext)
-        [base_name, version_name].compact.join('_') + ext
-      end
-
-      def full_original_filename
-        parent_name = super
-        ext         = File.extname(parent_name)
-        base_name   = parent_name.chomp(ext)
-        [base_name, version_name].compact.join('_') + ext
-      end
-    end
-  end
-end
