4f0ba84c307c924627168cbc5be8824c1c0c5d83
Brett Walker <brett@digitalmoksha.com>
2013-08-30 22:58:27 +0200

    split account editing into several screens

--- app/controllers/dm_core/admin/accounts_controller.rb
+++ app/controllers/dm_core/admin/accounts_controller.rb
@@ -2,31 +2,42 @@ class DmCore::Admin::AccountsController < DmCore::Admin::AdminController
 
   before_filter   :account_lookup
 
-  # GET /admin/account or GET /admin/accounts/.json
   #------------------------------------------------------------------------------
   def show
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @account }
-    end
+    redirect_to dm_core.admin_account_general_path
   end
 
-  # GET /admin/account/edit
   #------------------------------------------------------------------------------
-  def edit
+  def general
+    if put_or_post?
+      if @account.update_attributes(params[:account])
+        redirect_to(dm_core.admin_account_general_path, notice: "Account was successfully updated.") and return
+      else
+        render action: "general"
+      end
+    end
   end
 
   # PUT /admin/account or PUT /admin/account/.json
   #------------------------------------------------------------------------------
-  def update
-    params[:account].delete(:preferred_smtp_password) if params[:account][:preferred_smtp_password].blank?
-    respond_to do |format|
+  def email
+    if put_or_post?
+      params[:account].delete(:preferred_smtp_password) if params[:account][:preferred_smtp_password].blank?
       if @account.update_attributes(params[:account])
-        format.html { redirect_to dm_core.admin_account_url, notice: "Account was successfully updated." }
-        format.json { head :no_content }
+        redirect_to(dm_core.admin_account_email_path, notice: "Account was successfully updated.") and return
       else
-        format.html { render action: "edit" }
-        format.json { render json: @account.errors, status: :unprocessable_entity }
+        render action: "email"
+      end
+    end
+  end
+
+  #------------------------------------------------------------------------------
+  def analytics
+    if put_or_post?
+      if @account.update_attributes(params[:account])
+        redirect_to(dm_core.admin_account_analytics_path, notice: "Account was successfully updated.") and return
+      else
+        render action: "analytics"
       end
     end
   end
