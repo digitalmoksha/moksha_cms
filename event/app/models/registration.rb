78749c0cb5c2a11c12db686c6b0ab2e31b61cbc4
Brett Walker <brett@digitalmoksha.com>
2013-09-14 19:58:40 +0200

    tweak

--- event/app/models/registration.rb
+++ event/app/models/registration.rb
@@ -218,8 +218,8 @@ class Registration < ActiveRecord::Base
   #------------------------------------------------------------------------------
   def paypal_ipn(notify)
     if notify.acknowledge
-      payment_history = PaymentHistory.find_by_transaction_id(notify.transaction_id) ||
-                        manual_payment( nil,
+      # payment_history = PaymentHistory.find_by_transaction_id(notify.transaction_id) ||
+      payment_history = manual_payment( nil,
                                         notify.amount.to_s,
                                         notify.currency,
                                         nil,
