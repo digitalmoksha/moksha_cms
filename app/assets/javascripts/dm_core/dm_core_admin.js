4583a4d359c6f128af542f277ba14e0a18f24021
Brett Walker <brett@digitalmoksha.com>
2013-09-27 16:12:52 -0500

    add ace editor javascripts

--- app/assets/javascripts/dm_core/dm_core_admin.js
+++ app/assets/javascripts/dm_core/dm_core_admin.js
@@ -1,4 +1,14 @@
-
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
+// GO AFTER THE REQUIRES BELOW.
+//
+//= require ace/ace.js
+//= require ace/theme-crimson_editor.js
+//= require ace/mode-markdown.js
+//= require ace/theme-textmate.js
+//= require aced-rails.js
 
 // Function for toggling the revealing of an item, through slide and fading
 //----------------------------------------------------------------
