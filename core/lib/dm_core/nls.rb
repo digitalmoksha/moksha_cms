c9a7f159da78c600d64337a2db4d61fcd4c707ff
Brett Walker <brett@digitalmoksha.com>
2013-10-27 19:07:37 +0100

    fix nls()

--- core/lib/dm_core/nls.rb
+++ core/lib/dm_core/nls.rb
@@ -7,15 +7,10 @@ module Nls
   # default scope the msg lookup to the current theme
   #------------------------------------------------------------------------------
   def nls(message_key, options = {})
-    if options.empty?
-      #--- look up message in the scope of current theme, fallback to parent theme
-      key       = "#{Account.current.current_theme}.#{message_key.to_s}"
-      fallback  = Account.current.parent_theme ? "#{Account.current.parent_theme}.#{message_key.to_s}" : "translation missing: #{key}"
-      I18n.t(key, default: fallback.to_sym).html_safe      
-    else
-      #--- options specified, pass it all through
-      I18n.t(message_key, options).html_safe
-    end
+    #--- look up message in the scope of current theme, fallback to parent theme
+    key       = "#{Account.current.current_theme}.#{message_key.to_s}"
+    fallback  = Account.current.parent_theme ? "#{Account.current.parent_theme}.#{message_key.to_s}" : "translation missing: #{key}"
+    I18n.t(key, options.merge(default: fallback.to_sym)).html_safe      
   end
 
   # Helper to generate the image_tag for a language flag.  
