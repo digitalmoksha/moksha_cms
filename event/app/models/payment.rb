68a41d011f5abd1cd9fe83f4675242f34837e630
Brett Walker <brett@digitalmoksha.com>
2013-11-03 00:36:22 +0100

    fix logger error

--- event/app/models/payment.rb
+++ event/app/models/payment.rb
@@ -6,13 +6,13 @@ class Payment
   # Handle Payment notification logic
   #------------------------------------------------------------------------------
   def self.event_payment_ipn(notify, payment_method = '')
-    logger.error('===> Enter: Payment.event_payment_ipn')
-    logger.error(notify.inspect)
+    Rails.logger.error('===> Enter: Payment.event_payment_ipn')
+    Rails.logger.error(notify.inspect)
     registration = Registration.find_by_receipt_code(notify.item_id)
     
     if notify.acknowledge
       if registration
-        logger.error(registration.inspect)
+        Rails.logger.error(registration.inspect)
         payment_history = PaymentHistory.find_by_transaction_id(notify.transaction_id) ||
                             registration.manual_payment( nil,
                                           notify.amount.to_f.to_s,
@@ -24,13 +24,13 @@ class Payment
                                           transaction_id: notify.transaction_id,
                                           status: notify.status
                       )
-          logger.error(payment_history.inspect)
+          Rails.logger.error(payment_history.inspect)
         begin
           if notify.complete?
             payment_history.status = notify.status
           else
             # TODO need to handle refunding, etc
-            logger.error("Failed to verify #{payment_method} payment notification, please investigate")
+            Rails.logger.error("Failed to verify #{payment_method} payment notification, please investigate")
           end
         rescue => e
           payment_history.status = 'Error'
@@ -40,7 +40,7 @@ class Payment
         end
       else
         #--- [todo] a linked registration was not found.  Should be stored in payment table anyway
-        logger.error("   > Error: Registration was not found: #{notify.item_id}")
+        Rails.logger.error("   > Error: Registration was not found: #{notify.item_id}")
       end
     end
   end
