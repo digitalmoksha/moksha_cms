18fb026575692078ae8e7e11d0eaea0b14cbd3c5
Brett Walker <brett@digitalmoksha.com>
2013-11-30 17:47:04 +0100

    enable user deletion for sysadmin

--- core/config/initializers/_dm_core.rb
+++ core/config/initializers/_dm_core.rb
@@ -2,6 +2,6 @@
 if defined?(Rails::Console)
   puts "-------------------------------------------------------------------------------------"
   puts " App is Multi-Tenant.  Make sure to issue the following command before anything else."
-  puts "    account('specify_account_prefix')                                                "
+  puts "    Account.current_by_prefix('specify_account_prefix')                              "
   puts "-------------------------------------------------------------------------------------"
 end
