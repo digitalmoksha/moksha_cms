7ccb068be90cb8ac84c2053258fe58c2ca91ce7e
Brett Walker <brett@digitalmoksha.com>
2013-12-04 13:06:39 +0100

    fix model decorators and translation

--- event/config/initializers/dm_event.rb
+++ event/config/initializers/dm_event.rb
@@ -1,3 +1,1 @@
-require 'dm_event/model_decorators'
-
 ActiveMerchant::Billing::Base.mode = :test unless Rails.env.production?
--- event/lib/dm_event/engine.rb
+++ event/lib/dm_event/engine.rb
@@ -6,5 +6,10 @@ require 'activemerchant'
 module DmEvent
   class Engine < ::Rails::Engine
     isolate_namespace DmEvent
+
+    config.to_prepare do
+      require_dependency 'dm_event/model_decorators'
+    end
   end
 end
+
