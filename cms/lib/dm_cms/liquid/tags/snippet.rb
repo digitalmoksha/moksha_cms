95bb5625d5119c2e9573434017ccaa9812771685
Brett Walker <brett@digitalmoksha.com>
2014-02-17 16:46:13 +0100

    added snippet tag

---
+++ cms/lib/dm_cms/liquid/tags/snippet.rb
@@ -0,0 +1,37 @@
+#------------------------------------------------------------------------------
+module Liquid
+  class Snippet < DmCore::LiquidTag
+    include DmCore::AccountHelper
+    include DmCore::LiquidHelper
+    
+    #------------------------------------------------------------------------------
+    def render(context)
+      output = ''
+      if @attributes['slug'].present?
+        cms_snippet = CmsSnippet.find_by_slug(@attributes['slug'])
+        if cms_snippet
+          output = context.registers[:view].render_content_item(cms_snippet)          
+        end
+      end
+      return output
+    end
+
+    #------------------------------------------------------------------------------
+    def self.details
+      { name: self.tag_name,
+        summary: 'Display a snippet',
+        category: '',
+        description: <<-END_OF_DESCRIPTION
+Output the content of a snippet specified by the slug
+
+~~~
+{% snippet slug: 'some-snippet-slug'}
+~~~
+
+END_OF_DESCRIPTION
+      }
+    end
+  end
+  Template.register_tag('snippet', Snippet)
+end
+
