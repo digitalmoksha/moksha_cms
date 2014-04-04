0997bfdd5d47e6e0bb6c79f5945cdad0e560bb71
Brett Walker <brett@digitalmoksha.com>
2014-04-04 09:59:17 +0200

    rename the index instead of trying to add a new one

--- cms/db/migrate/20140217135228_rename_snippet_slug.rb
+++ cms/db/migrate/20140217135228_rename_snippet_slug.rb
@@ -2,12 +2,12 @@ class RenameSnippetSlug < ActiveRecord::Migration
   def up
     change_column :cms_snippets, :itemtype, :string, :default => 'markdown'
     rename_column :cms_snippets, :container, :slug
-    add_index     :cms_snippets, :slug
+    rename_index  :cms_snippets, 'index_cms_snippets_on_container', 'index_cms_snippets_on_slug'
   end
 
   def down
     change_column :cms_snippets, :itemtype, :string, :default => 'textile'
     rename_column :cms_snippets, :slug, :container
-    remove_index  :cms_snippets, :slug
+    rename_index  :cms_snippets, 'index_cms_snippets_on_slug', 'index_cms_snippets_on_container'
   end
 end
