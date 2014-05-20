18ee622ee779e64680d34da4d1dccbe739de0539
Brett Walker <brett@digitalmoksha.com>
2014-05-20 11:55:05 +0200

    fix simple_form init for bootstrap 2 support

--- core/config/initializers/simple_form_bootstrap.rb
+++ core/config/initializers/simple_form_bootstrap.rb
@@ -1,76 +1,87 @@
 # Configured for use with Bootstrap 2
 #------------------------------------------------------------------------------
 
-# Use this setup block to configure all options available in SimpleForm.
 SimpleForm.setup do |config|
-  #--- Note: by default we use the 'form-horizontal' on all form fields. However,
-  #    in order to allow certain fields to be 'vertical' (have the label text
-  #    appear above the field, default form format), we apply the 
-  #    'form-horizontal' class to each control group, not the form.  We then have a special
-  #    'vertical' wrapper that removes this class, giving vertical behaviour
-  config.wrappers :bootstrap2, :tag => 'div', :class => 'form-horizontal control-group', :error_class => 'error' do |b|
+  config.wrappers :bs2_horizontal_form, tag: 'div', class: 'form-horizontal control-group', error_class: 'error' do |b|
     b.use :html5
     b.use :placeholder
-    b.use :label
-    b.wrapper :tag => 'div', :class => 'controls' do |ba|
+    b.use :label, class: 'control-label'
+    b.wrapper tag: 'div', class: 'controls' do |ba|
       ba.use :input
-      ba.use :error, :wrap_with => { :tag => 'span', :class => 'help-inline' }
-      ba.use :hint,  :wrap_with => { :tag => 'p', :class => 'help-block' }
+      ba.use :error, wrap_with: { tag: 'span', class: 'help-inline' }
+      ba.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
     end
   end
 
-  config.wrappers :bs2_prepend, :tag => 'div', :class => "form-horizontal control-group", :error_class => 'error' do |b|
+  #--- same as ::bs2_horizontal_form wrapper, but here for backward compatability
+  config.wrappers :bootstrap2, tag: 'div', class: 'form-horizontal control-group', error_class: 'error' do |b|
+    b.use :html5
+    b.use :placeholder
+    b.use :label, class: 'control-label'
+    b.wrapper tag: 'div', class: 'controls' do |ba|
+      ba.use :input
+      ba.use :error, wrap_with: { tag: 'span', class: 'help-inline' }
+      ba.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
+    end
+  end
+
+  config.wrappers :bs2_vertical_form, tag: 'div', error_class: 'error' do |b|
     b.use :html5
     b.use :placeholder
     b.use :label
-    b.wrapper :tag => 'div', :class => 'controls' do |input|
-      input.wrapper :tag => 'div', :class => 'input-prepend' do |prepend|
+    b.wrapper tag: 'div' do |ba|
+      ba.use :input
+      ba.use :error, wrap_with: { tag: 'span', class: 'help-inline' }
+      ba.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
+    end
+  end
+
+  config.wrappers :bs2_prepend, tag: 'div', class: "form-horizontal control-group", error_class: 'error' do |b|
+    b.use :html5
+    b.use :placeholder
+    b.use :label, class: 'control-label'
+    b.wrapper tag: 'div', class: 'controls' do |input|
+      input.wrapper tag: 'div', class: 'input-prepend' do |prepend|
         prepend.use :input
       end
-      input.use :hint,  :wrap_with => { :tag => 'span', :class => 'help-block' }
-      input.use :error, :wrap_with => { :tag => 'span', :class => 'help-inline' }
+      input.use :hint,  wrap_with: { tag: 'span', class: 'help-block' }
+      input.use :error, wrap_with: { tag: 'span', class: 'help-inline' }
     end
   end
 
-  config.wrappers :bs2_append, :tag => 'div', :class => "form-horizontal control-group", :error_class => 'error' do |b|
+  config.wrappers :bs2_append, tag: 'div', class: "form-horizontal control-group", error_class: 'error' do |b|
     b.use :html5
     b.use :placeholder
-    b.use :label
-    b.wrapper :tag => 'div', :class => 'controls' do |input|
-      input.wrapper :tag => 'div', :class => 'input-append' do |append|
+    b.use :label, class: 'control-label'
+    b.wrapper tag: 'div', class: 'controls' do |input|
+      input.wrapper tag: 'div', class: 'input-append' do |append|
         append.use :input
       end
-      input.use :hint,  :wrap_with => { :tag => 'span', :class => 'help-block' }
-      input.use :error, :wrap_with => { :tag => 'span', :class => 'help-inline' }
+      input.use :hint,  wrap_with: { tag: 'span', class: 'help-block' }
+      input.use :error, wrap_with: { tag: 'span', class: 'help-inline' }
     end
   end
 
-  config.wrappers :bs2_prepend_append, :tag => 'div', :class => "form-horizontal control-group", :error_class => 'error' do |b|
+  config.wrappers :bs2_prepend_append, tag: 'div', class: "form-horizontal control-group", error_class: 'error' do |b|
     b.use :html5
     b.use :placeholder
-    b.use :label
-    b.wrapper :tag => 'div', :class => 'controls' do |input|
-      input.wrapper :tag => 'div', :class => 'input-prepend input-append' do |append|
+    b.use :label, class: 'control-label'
+    b.wrapper tag: 'div', class: 'controls' do |input|
+      input.wrapper tag: 'div', class: 'input-prepend input-append' do |append|
         append.use :input
       end
-      input.use :hint,  :wrap_with => { :tag => 'span', :class => 'help-block' }
-      input.use :error, :wrap_with => { :tag => 'span', :class => 'help-inline' }
+      input.use :hint,  wrap_with: { tag: 'span', class: 'help-block' }
+      input.use :error, wrap_with: { tag: 'span', class: 'help-inline' }
     end
   end
 
-  config.wrappers :bs2_inline_checkbox, :tag => 'div', :class => 'form-horizontal control-group', :error_class => 'error' do |b|
+  config.wrappers :bs2_inline_checkbox, tag: 'div', class: 'form-horizontal control-group', error_class: 'error' do |b|
     b.use :html5
     b.use :placeholder
-    b.wrapper :tag => 'div', :class => 'controls' do |ba|
-      ba.use :label_input, :wrap_with => { :class => 'checkbox inline' }
-      ba.use :error, :wrap_with => { :tag => 'span', :class => 'help-inline' }
-      ba.use :hint,  :wrap_with => { :tag => 'p', :class => 'help-block' }
+    b.wrapper tag: 'div', class: 'controls' do |ba|
+      ba.use :label_input, wrap_with: { class: 'checkbox inline' }
+      ba.use :error, wrap_with: { tag: 'span', class: 'help-inline' }
+      ba.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
     end
   end
-      
-  # Wrappers for forms and inputs using the Twitter Bootstrap toolkit.
-  # Check the Bootstrap docs (http://twitter.github.com/bootstrap)
-  # to learn about the different styles for forms and inputs,
-  # buttons and other elements.
-  #config.default_wrapper = :bootstrap2
 end
