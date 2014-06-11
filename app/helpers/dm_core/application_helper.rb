b1d0382c4c9df3d41b8a8d6d45fd53aef3618789
Brett Walker <brett@digitalmoksha.com>
2014-06-11 19:00:35 +0200

    display editable custom fields a little better

--- app/helpers/dm_core/application_helper.rb
+++ app/helpers/dm_core/application_helper.rb
@@ -52,7 +52,7 @@ module DmCore
       fields      = f.simple_fields_for(association, new_object, child_index: id) do |builder|
         render('dm_core/admin/custom_fields/' + association.to_s.singularize + "_fields", f: builder)
       end
-      link_to(name, '#', class: 'add_custom_fields', data: {id: id, fields: fields.gsub("\n", "")})
+      link_to(name, '#', class: 'add_custom_fields btn btn-xs btn-default', data: {id: id, fields: fields.gsub("\n", "")})
     end
   end
 end
