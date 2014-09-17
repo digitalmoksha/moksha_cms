d47c6a56cb676a614c01f49daeecab3730d85106
Brett Walker <brett@digitalmoksha.com>
2014-09-17 16:28:49 +0200

    can now specify column sizes in some simple_form inputs

--- config/initializers/simple_form_bootstrap3.rb
+++ config/initializers/simple_form_bootstrap3.rb
@@ -3,6 +3,10 @@ SimpleForm.setup do |config|
   config.button_class = 'btn btn-default'
   config.boolean_label_class = nil
 
+  #------------------------------------------------------------------------------
+  # Bootstrap 3 vertical form style
+  #------------------------------------------------------------------------------
+  
   config.wrappers :bs3_vertical_form, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
     b.use :html5
     b.use :placeholder
@@ -60,12 +64,16 @@ SimpleForm.setup do |config|
     b.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
   end
 
+  #------------------------------------------------------------------------------
+  # Bootstrap 3 horizontal form style
+  #------------------------------------------------------------------------------
+  
   config.wrappers :bs3_horizontal_form, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
     b.use :html5
     b.use :placeholder
     b.use :label, class: 'col-sm-2 control-label'
 
-    b.wrapper tag: 'div', class: 'col-sm-10' do |ba|
+    b.wrapper :input_wrapper, tag: 'div', class: 'col-sm-10' do |ba|
       ba.use :input, class: 'form-control'
       ba.use :error, wrap_with: { tag: 'span', class: 'help-block' }
       ba.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
@@ -124,6 +132,40 @@ SimpleForm.setup do |config|
     end
   end
 
+  #------------------------------------------------------------------------------
+  # Bootstrap 3 horizontal form style
+  # version that allows you to specify the column sizes to use
+  #------------------------------------------------------------------------------
+  
+  # f.input :email, label_html: {class: 'col-sm-5'}, input_wrapper_html: {class: 'col-sm-6'}
+  config.wrappers :bs3_horizontal_nosize, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
+    b.use :html5
+    b.use :placeholder
+    b.use :label, class: 'control-label'
+
+    b.wrapper :input_wrapper, tag: 'div' do |ba|
+      ba.use :input, class: 'form-control'
+      ba.use :error, wrap_with: { tag: 'span', class: 'help-block' }
+      ba.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
+    end
+  end
+
+  # f.input :remember_me, as: :boolean, input_wrapper_html: {class: 'col-sm-offset-4 col-sm-6'} %>
+  config.wrappers :bs3_horizontal_nosize_boolean, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
+    b.use :html5
+    b.use :placeholder
+
+    b.wrapper :input_wrapper, tag: 'div' do |wr|
+      wr.wrapper tag: 'div', class: 'checkbox' do |ba|
+        ba.use :label_input
+      end
+
+      wr.use :error, wrap_with: { tag: 'span', class: 'help-block' }
+      wr.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
+    end
+  end
+
+   
   # Wrappers for forms and inputs using the Bootstrap toolkit.
   # Check the Bootstrap docs (http://getbootstrap.com)
   # to learn about the different styles for forms and inputs,
