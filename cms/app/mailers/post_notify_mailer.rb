8aabb4fe155b1f90478c974359512e5174e4ac3e
Brett Walker <brett@digitalmoksha.com>
2014-12-16 19:37:22 +0100

    use correct header_image

--- cms/app/mailers/post_notify_mailer.rb
+++ cms/app/mailers/post_notify_mailer.rb
@@ -7,24 +7,28 @@ class PostNotifyMailer < DmCore::SiteMailer
 
   layout 'email_templates/dm_cms_email_layout'
 
+  # send notification email, using the users preferred locale if possible
   #------------------------------------------------------------------------------
   def post_notify(user, post)
-    account                     = post.account
-    @subject                    = "Blog: #{post.cms_blog.title} :: #{post.title}"
-    @recipients                 = user.email
-    @blog_title                 = post.cms_blog.title
-    @post_title                 = post.title
-    @post_link                  = dm_cms.post_show_url(post.cms_blog.slug, post.slug, locale: I18n.locale)
-    @post                       = post
-    @header_image               = post.cms_blog.image_email_header || post.cms_blog.image
+    account                       = post.account
+    locale                        = account.verify_locale(user.locale)
+    I18n.with_locale(locale) do
+      @subject                    = "Blog: #{post.cms_blog.title} :: #{post.title}"
+      @recipients                 = user.email
+      @blog_title                 = post.cms_blog.title
+      @post_title                 = post.title
+      @post_link                  = dm_cms.post_show_url(post.cms_blog.slug, post.slug, locale: locale)
+      @post                       = post
+      @header_image               = post.cms_blog.image_email_header || post.cms_blog.header_image
 
-    headers = { "Return-Path" => account.preferred_blog_from_email || account.preferred_smtp_from_email }
-    mail( from: account.preferred_blog_from_email || account.preferred_smtp_from_email,
-          reply_to: account.preferred_blog_from_email || account.preferred_smtp_from_email,
-          to: @recipients, subject: @subject, 
-          theme: account.account_prefix) do |format|
-      format.text { render "layouts/email_templates/dm_cms_post_notify.text.erb" }
-      format.html { render "layouts/email_templates/dm_cms_post_notify.html.erb" }
+      headers = { "Return-Path" => account.preferred_blog_from_email || account.preferred_smtp_from_email }
+      mail( from: account.preferred_blog_from_email || account.preferred_smtp_from_email,
+            reply_to: account.preferred_blog_from_email || account.preferred_smtp_from_email,
+            to: @recipients, subject: @subject, 
+            theme: account.account_prefix) do |format|
+        format.text { render "layouts/email_templates/dm_cms_post_notify.text.erb" }
+        format.html { render "layouts/email_templates/dm_cms_post_notify.html.erb" }
+      end
     end
   end
 
