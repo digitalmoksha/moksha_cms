861da71727872ba368d5a4120d018249b1982e22
Brett Walker <brett@digitalmoksha.com>
2015-04-16 16:01:34 +0200

    page_link now accepts optional text, and
    defaults to the page's menutitle

--- cms/app/helpers/dm_cms/pages_helper.rb
+++ cms/app/helpers/dm_cms/pages_helper.rb
@@ -161,9 +161,9 @@ module DmCms::PagesHelper
   
   # return a link to the page's slug, with the passed in link text
   #------------------------------------------------------------------------------
-  def page_link(page, text)
-    text ||= ''
-    link_to text.html_safe, dm_cms.showpage_url(page.slug)
+  def page_link(page, text = nil)
+    text ||= page.menutitle
+    link_to text, dm_cms.showpage_url(page.slug)
   end
   
   private
