0195e7b1302aeb6f6f6b8e90c498e9895a50a5c8
Brett Walker <brett@digitalmoksha.com>
2016-02-02 18:26:31 +0100

    handle possible missing value during migration

--- core/db/migrate/20141216094639_add_favored_locale.rb
+++ core/db/migrate/20141216094639_add_favored_locale.rb
@@ -3,7 +3,9 @@ class AddFavoredLocale < ActiveRecord::Migration
     add_column :user_profiles, :favored_locale, :string
 
     locale_map = {'DE' => :de, 'AT' => :de, 'CH' => :de, 'JP' => :ja, 'FI' => :fi}
+    us_country = DmCore::Country.find_by_code('US')
     UserProfile.all.find_each do |user_profile|
+      user_profile.update_attribute(:country, us_country) if user_profile.country.nil?
       country_code = user_profile.country.code
       locale = locale_map[country_code].nil? ? :en : locale_map[country_code]
       user_profile.update_attribute(:favored_locale, locale)
