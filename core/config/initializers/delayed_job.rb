0928bed89c1e3546c6d1e9720ef5a4362400687d
Brett Walker <brett@digitalmoksha.com>
2016-06-30 12:55:18 +0200

    move delayed_job init to the main app

--- core/config/initializers/delayed_job.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-require 'delayed_job'
-
-Rails.application.config.active_job.queue_adapter = :delayed_job
-
-Delayed::Worker.destroy_failed_jobs = false
-Delayed::Worker.max_attempts = 3
-Delayed::Worker.max_run_time = 4.hours
-Delayed::Worker.default_queue_name = 'default'
