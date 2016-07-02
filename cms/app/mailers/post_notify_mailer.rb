d1c05e3f7ce65e0ec029506c74f6f1ef43b2f087
Brett Walker <brett@digitalmoksha.com>
2016-07-02 22:19:57 +0200

    added host and protocol to url in mailer

--- cms/app/mailers/post_notify_mailer.rb
+++ cms/app/mailers/post_notify_mailer.rb
@@ -11,13 +11,14 @@ class PostNotifyMailer < DmCore::SiteMailer
   #------------------------------------------------------------------------------
   def post_notify(user, post, account)
     Account.current               = account  # needed so this can run in a background job
+    # Account.current.set_default_values
     locale                        = account.verify_locale(user.locale)
     I18n.with_locale(locale) do
       @subject                    = "Blog: #{post.cms_blog.title} :: #{post.title}"
       @recipients                 = user.email
       @blog_title                 = post.cms_blog.title
       @post_title                 = post.title
-      @post_link                  = dm_cms.post_show_url(post.cms_blog.slug, post.slug, locale: locale)
+      @post_link                  = dm_cms.post_show_url(post.cms_blog.slug, post.slug, locale: locale, host: account.url_host, protocol: account.url_protocol)
       @post                       = post
       @header_image               = post.cms_blog.image_email_header || post.cms_blog.header_image
 
