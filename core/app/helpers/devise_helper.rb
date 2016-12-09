8d99dee4e032067a60e05d27c1ab0944021b15f5
Brett Walker <brett@digitalmoksha.com>
2016-12-09 15:26:21 +0100

    refactored the recaptcha view code into a helper

--- core/app/helpers/devise_helper.rb
+++ core/app/helpers/devise_helper.rb
@@ -5,8 +5,9 @@ module DeviseHelper
   #
   # This method is intended to stay simple and it is unlikely that we are going to change
   # it to add more behavior or options.
+  #------------------------------------------------------------------------------
   def devise_error_messages!
-    return "" if resource.errors.empty?
+    return '' if resource.errors.empty?
 
     messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
     sentence = I18n.t("errors.messages.not_saved",
@@ -20,6 +21,22 @@ module DeviseHelper
     </div>
     HTML
 
+    html.html_safe
+  end
+
+  #------------------------------------------------------------------------------
+  def devise_recaptcha
+    return '' unless Rails.application.secrets[:recaptcha_site_key]
+    error = flash[:recaptcha_error] ? "<span class='help-inline'>#{flash[:recaptcha_error]}</span>" : ''
+    html  = <<-HTML
+    <div class="control-group #{'error' if flash[:recaptcha_error]}">
+      <div class="controls">
+        #{recaptcha_tags(site_key: Rails.application.secrets[:recaptcha_site_key], hl: I18n.locale)}
+        #{error}
+      </div>
+    </div>
+    HTML
+    
     html.html_safe
   end
 end
