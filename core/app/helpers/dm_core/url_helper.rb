5f65cb95f865d3fead5bff9e9a9eff92b07dcbda
Brett Walker <brett@digitalmoksha.com>
2017-07-31 16:49:36 +0200

    upgrade to using aws-sdk v2

--- core/app/helpers/dm_core/url_helper.rb
+++ core/app/helpers/dm_core/url_helper.rb
@@ -42,23 +42,22 @@ module DmCore
     #   s3s://bucket_name/object_name?expires=20     => SSL, expires in 20 minutes
     #   s3s://bucket_name/object_name?expires=public => links directly to file (it must
     #      be a World readable file, and the link will never expire)
+    # All urls are SSL
     #------------------------------------------------------------------------------
     def s3_generate_expiring_link(url)
       access_key  = Account.current.theme_data['AWS_ACCESS_KEY_ID']
       secret_key  = Account.current.theme_data['AWS_SECRET_ACCESS_KEY']
+      region      = Account.current.theme_data['AWS_REGION']
       uri         = URI.parse(url)
-      secure      = (uri.scheme == 's3s')
       bucket      = uri.host
       object_name = uri.path.gsub(/^\//, '')
       expire_mins = (uri.query.blank? ? nil : CGI::parse(uri.query)['expires'][0]) || '10'
-      
-      if expire_mins == "public"
-        "#{secure ? 'https' : 'http'}://#{bucket}.s3.amazonaws.com/#{object_name}"
-      else
-        s3 = ::AWS::S3.new(access_key_id: access_key, secret_access_key: secret_key)
-        object = s3.buckets[bucket].objects[object_name]
-        object.url_for(:get, {expires: expire_mins.to_i.minutes.from_now, secure: secure}).to_s
-      end
+      expire_secs = expire_mins.to_i.minutes.to_i  # will be 0 if 'public' or some other non-integer string
+      client      = Aws::S3::Client.new(access_key_id: access_key, secret_access_key: secret_key, region: region)
+      s3          = Aws::S3::Resource.new(client: client)
+      object      = s3.bucket(bucket).object(object_name)
+
+      expire_secs == 0 ? object.public_url : object.presigned_url(:get, expires_in: expire_secs)
     end
 
     # if a relative url path is given, then expand it by prepending the supplied 
