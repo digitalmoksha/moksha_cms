15621df9a03fde8c2f58980963ef8934126db58a
Brett Walker <brett@digitalmoksha.com>
2017-08-07 16:26:01 +0200

    because other plugins or gems may want to upload files into the `uploads` folder, put all the cms uploaded files into `/site_assets/uploads/[account_prefix/media`.  Those gems can now upload into their own specific folder if they wish

--- core/app/helpers/dm_core/account_helper.rb
+++ core/app/helpers/dm_core/account_helper.rb
@@ -16,64 +16,98 @@ module DmCore
       current_account.account_prefix
     end
 
+    # Site specific assets (such as static content in the repository, and user uploaded files)
+    # are stored in the `/site_assets` directory:
+    #  /site_assets
+    #      content
+    #         [account_prefix]
+    #            images, stylesheets, etc (these items are typically checked into the repo)
+    #      uploads
+    #         [account_prefix]
+    #            media (files uploaded by the content management system for blogs, pages, etc)
+    #            directories for other uploaders, maybe for online courses, etc.
+    #  /protected_asset
+    #      uploads
+    #         [account_prefix]
+    #            media (files uploaded by the content management system that are to be access protected)
+    #
+    # The following methods provide the various paths to the directories
+
     # Returns the path (from the root of the site) to the base site specific directory
-    #   Pass in false not to include leading slash
+    # eg: /site_assets
     #------------------------------------------------------------------------------
     def account_site_assets_base(leading_slash = true)
       leading_slash ? "/#{SITE_ASSETS_FOLDER}" : SITE_ASSETS_FOLDER
     end
 
-    #------------------------------------------------------------------------------
-    def account_protected_assets_base(leading_slash = true)
-      leading_slash ? "/#{PROTECTED_ASSET_TRIGGER}" : PROTECTED_ASSET_TRIGGER
-    end
-
-    #------------------------------------------------------------------------------
-    def account_protected_assets_folder
-      "#{Rails.root}/#{PROTECTED_ASSETS_FOLDER}"
-    end
-
-    # Returns the path (from the root of the site) to the site general asset files
-    #   Pass in false not to include leading slash
+    # Returns the path (from the root of the site) to the content asset files
+    # eg: /site_assets/content/[account_prefix]
     #------------------------------------------------------------------------------
     def account_site_assets(leading_slash = true)
       "#{account_site_assets_base(leading_slash)}/content/#{account_prefix}"
     end
 
-    # Returns the path (from the root of the site) to the protected asset files
+    # Returns the path (from the root of the site) to the uploads directory
+    # eg: /site_assets/uploads/[account_prefix]
     #------------------------------------------------------------------------------
-    def account_protected_assets_media_folder
-      "#{account_protected_assets_folder}/uploads/#{account_prefix}"
-    end
-
-    # Returns the path (from the root of the site) to the site's uploadable asset
-    # folder, which is 'media'
-    #   Pass in false not to include leading slash
-    #------------------------------------------------------------------------------
-    def account_site_assets_media(leading_slash = true)
+    def account_site_assets_uploads(leading_slash = true)
       "#{account_site_assets_base(leading_slash)}/uploads/#{account_prefix}"
     end
 
-    # Returns the url of the site's base site specific directory
-    #   Pass in false not to include leading slash
+    # Returns the path (from the root of the site) to the site's uploadable asset
+    # folder for the CMS, which is 'media'
+    # eg: /site_assets/uploads/[account_prefix]/media
+    #------------------------------------------------------------------------------
+    def account_site_assets_media(leading_slash = true)
+      "#{account_site_assets_uploads(leading_slash)}/media"
+    end
+
+    # Returns the url to the base site specific directory
+    # eg: http://mysite.com/site_assets
     #------------------------------------------------------------------------------
     def account_site_assets_base_url
       Account.current.url_base + account_site_assets_base(true)
     end
 
     # Returns the url of the site's general asset files
-    #   Pass in false not to include leading slash
+    # eg: http://mysite.com/site_assets/content/[account_prefix]
     #------------------------------------------------------------------------------
     def account_site_assets_url
       Account.current.url_base + account_site_assets(true)
     end
 
     # Returns the url of the site's media asset files
-    #   Pass in false not to include leading slash
+    # eg: http://mysite.com/site_assets/uploads/[account_prefix]/media
     #------------------------------------------------------------------------------
     def account_site_assets_media_url
       Account.current.url_base + account_site_assets_media(true)
     end
+
+
+
+    # Returns the path (from the root of the site) to the protected asset directory
+    # It is built with the trigger word, `protected_asset`, which will trigger the
+    # special route that adds the protection
+    # eg: /protected_asset
+    #------------------------------------------------------------------------------
+    def account_protected_assets_base(leading_slash = true)
+      leading_slash ? "/#{PROTECTED_ASSET_TRIGGER}" : PROTECTED_ASSET_TRIGGER
+    end
+
+    # the actual folder on the webserver where the protected files are stored
+    # eg: [Rails.root]/protected_assets
+    #------------------------------------------------------------------------------
+    def account_protected_assets_folder
+      "#{Rails.root}/#{PROTECTED_ASSETS_FOLDER}"
+    end
+
+    # the actual folder on the webserver where the protected media files are stored
+    # eg: [Rails.root]/protected_assets/uploads/[account_prefix]/media
+    #------------------------------------------------------------------------------
+    def account_protected_assets_media_folder
+      "#{account_protected_assets_folder}/uploads/#{account_prefix}/media"
+    end
+
 
   private
   
--- core/lib/dm_core/version.rb
+++ core/lib/dm_core/version.rb
@@ -1,3 +1,3 @@
 module DmCore
-  VERSION = "5.0.20"
+  VERSION = "5.0.21"
 end
