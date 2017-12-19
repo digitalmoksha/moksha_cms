b9701e68956e691fc82c2229716461f4d8c49653
digitalMoksha <brett@digitalmoksha.com>
2017-12-19 18:47:11 +0100

    prompt user for account prefix in Rails console

--- core/config/initializers/_dm_core.rb
+++ core/config/initializers/_dm_core.rb
@@ -4,4 +4,8 @@ if defined?(Rails::Console)
   puts " App is Multi-Tenant.  Make sure to issue the following command before anything else."
   puts "    Account.current_by_prefix('specify_account_prefix')                              "
   puts "-------------------------------------------------------------------------------------"
+  puts "Input your account prefix:"
+  prefix = gets.chomp
+  puts "--> Account.current_by_prefix('#{prefix}')"
+  Account.current_by_prefix(prefix)
 end
