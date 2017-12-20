e8d8262236df7718f0e755c74cf5d9944eccc36e
digitalMoksha <brett@digitalmoksha.com>
2017-12-20 08:21:03 +0100

    fix deprecation warning for Devise::TestHelpers

--- core/spec/support/devise.rb
+++ core/spec/support/devise.rb
@@ -49,7 +49,7 @@ end
 #------------------------------------------------------------------------------
 RSpec.configure do |config|
   config.include  Devise::Test::ControllerHelpers,  type: :controller
-  config.include  Devise::TestHelpers,              type: :helper
+  config.include  Devise::Test::ControllerHelpers,  type: :helper
   config.include  Devise::Test::ControllerHelpers,  type: :view
   config.include  Devise::Test::IntegrationHelpers, type: :feature
   config.include  Warden::Test::Helpers,            type: :request
