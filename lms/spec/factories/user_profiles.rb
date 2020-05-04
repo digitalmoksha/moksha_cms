170c9733b41e95c9c61a51d38672ada83de4f0a5
digitalMoksha <brett@digitalmoksha.com>
2020-05-04 10:44:01 -0500

    Propagate updated user factories

--- lms/spec/factories/user_profiles.rb
+++ lms/spec/factories/user_profiles.rb
@@ -6,5 +6,6 @@ FactoryBot.define do
     last_name       { Faker::Name.last_name }
     public_name     { Faker::Name.first_name }
     email           { Faker::Internet.email }
+    country         { create(:country) }
   end
 end
