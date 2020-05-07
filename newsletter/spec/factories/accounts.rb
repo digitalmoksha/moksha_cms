8ee620a034c92f23a604a538541ceacb60b6b8bd
digitalMoksha <brett@digitalmoksha.com>
2020-05-07 17:05:53 -0500

    Get newsletter specs working

--- newsletter/spec/factories/accounts.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :account do
-    domain                      { 'test.example.com' }
-    account_prefix              { 'test' }
-    preferred_default_currency  { 'EUR' }
-  end
-end
