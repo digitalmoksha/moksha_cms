db9ceccdee5a78a82cef251e413011a869836ebc
digitalMoksha <brett@digitalmoksha.com>
2020-05-07 15:49:33 -0500

    Update common dependencies

--- common_gemfile_dependencies.rb
+++ common_gemfile_dependencies.rb
@@ -9,35 +9,33 @@ gem 'dm_preferences',         '~> 1.5'
 gem 'themes_for_rails',       git: 'https://github.com/digitalmoksha/themes_for_rails.git'
 gem 'aced_rails',             git: 'https://github.com/digitalmoksha/aced_rails.git'
 
-gem 'sass-rails', '~> 5.0'
-gem 'uglifier', '>= 1.3.0'
+gem 'sass-rails', '~> 6.0'
+gem 'uglifier', '>= 4.2.0'
 gem 'coffee-rails', '~> 4.2'
 gem 'therubyracer', platforms: :ruby
 
 group :development, :test do
-  gem 'sqlite3', '~> 1.3.6' # until an ActiveRecrod dependecy is removed
+  gem 'sqlite3', '~> 1.4.2'
   gem 'pry-byebug'
   gem 'thin' # use the Thin webserver during development
 
-  gem 'mocha', '~> 1.2', require: false
-  gem 'rspec-rails', '~> 3.5'
+  gem 'mocha', '~> 1.11', require: false
+  gem 'rspec-rails', '~> 4.0'
   gem 'factory_bot_rails', '~> 5.2'
 
   gem 'rubocop', '~> 0.52'
   gem 'rubocop-rspec', '~> 1.22'
-
-  gem 'bundler-audit', git: 'https://github.com/rubysec/bundler-audit'
 end
 
 group :test do
-  gem 'faker', '~> 1.7'
-  gem 'capybara', '~> 2.13'
-  gem 'database_cleaner', '~> 1.5'
-  gem 'launchy', '~> 2.4'
-  gem 'selenium-webdriver', '~> 3.3'
+  gem 'faker', '~> 2.11'
+  gem 'capybara', '~> 3.32'
+  gem 'database_cleaner', '~> 1.8'
+  gem 'launchy', '~> 2.5'
+  gem 'selenium-webdriver', '~> 3.142'
   gem 'rspec-formatter-webkit'
   gem 'rails-controller-testing'
-  gem 'shoulda-matchers', git: 'https://github.com/thoughtbot/shoulda-matchers.git', branch: 'rails-5'
+  gem 'shoulda-matchers'
   gem 'syntax'
-  gem 'webmock', '~> 3.7'
+  gem 'webmock', '~> 3.8'
 end
